4191bd0e1970b706df103a7a87b0cc1f4e411cdc
Sunil Das <95643764+sunildas1@users.noreply.github.com>
2025-08-14 21:17:21 +0600

    complete a database: created, updated, deleted

---
+++ config/inngest.js
@@ -0,0 +1,62 @@
+import { Inngest } from "inngest";
+import connectDB from "./db";
+import User from "@/models/User";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "quickshop-next" });
+
+// Inngest function to save data and database
+
+export const syncUserCreation = inngest.createFunction(
+    {
+        id: 'sync-user-from-clek'
+    },
+    { event: 'clek/user.created' },
+    async ({event}) =>{
+        const {id, first_name, last_name, email_addresses, image_url} = event.data
+        const userData = {
+            _id: id,
+            name: first_name + ' ' + last_name,
+            email: email_addresses[0].email_address,
+            imageUrl: image_url
+        }
+        await connectDB()
+        await User.create(userData)
+    }
+)
+
+// Inngest function to update user data in datebase
+
+export const syncUserUpdation = inngest.createFunction(
+    {
+        id: 'update-user-from-clek'
+    },
+    { event: 'clek/user.updated' },
+    async ({event}) =>{
+        const {id, first_name, last_name, email_addresses, image_url} = event.data
+        const userData = {
+            _id: id,
+            name: first_name + ' ' + last_name,
+            email: email_addresses[0].email_address,
+            imageUrl: image_url
+        }
+        await connectDB()
+        await User.findByIdAndUpdate(id, userData)
+    }
+)
+
+ // Inngest function to delete user from datebase
+
+ export const syncUserDeletion = inngest.createFunction(
+    {
+        id: 'delete-user-from-clek'
+    },
+    { event: 'clek/user.deleted' },
+    async ({event}) =>{
+        const {id} = event.data
+
+        await connectDB()
+        await User.findByIdAndDelete(id)
+    }
+    
+)
